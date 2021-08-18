bbf3e260d5125a5b2f9cf7c1565db204d72cdf77
unknown <shankiflang@gmail.com>
2021-08-19 00:00:13 +0200

    Updating Login and Register services

--- backend/authService/src/services/auth.ts
+++ backend/authService/src/services/auth.ts
@@ -1,21 +1,68 @@
-import { Service, Inject } from 'typedi'
-import config from '@config/index'
-import { randomBytes } from 'crypto'
-import { IUser, IUserInputDTO } from '@interfaces/IUser'
+import { getToken, getRefreshToken } from '@src/authenticate'
+import { ISession, IUser, IUserInputDTO } from '@interfaces/IUser'
+import { Service } from 'typedi'
+import User from '@src/models/user'
 
 @Service()
 export default class AuthService {
-    constructor(@Inject('userModel') private userModel: Models.UserModel) {}
+    public async Register(userInput: IUserInputDTO): Promise<{
+        success: boolean
+        user: IUser
+        refreshToken: ISession
+        token
+    }> {
+        try {
+            let user = await User.register(new User({ email: userInput.email }), userInput.password)
 
-    public async Register(
+            if (!user) throw new Error('User already exists')
+
+            const token = getToken({ _id: user._id })
+            const refreshToken = getRefreshToken({ _id: user._id })
+
+            user.refreshToken.push({ refreshToken })
+            user = await user.save((error, user) => {
+                if (error) throw new Error(error)
+            })
+
+            return { success: true, user, refreshToken, token }
+        } catch (error) {
+            throw error
+        }
+    }
+
+    public async Login(userInput: IUser): Promise<{
+        success: boolean
+        user: IUser
+        refreshToken: ISession
+        token
+    }> {
+        try {
+            let user = await User.findById(userInput._id)
+
+            if (!user) throw new Error('User not found')
+
+            const token = getToken({ _id: userInput._id })
+            const refreshToken = getRefreshToken({ _id: userInput._id })
+
+            user.refreshToken.push({ refreshToken })
+            user = await user.save((error, user) => {
+                if (error) throw new Error(error)
+            })
+
+            return { success: true, user, refreshToken, token }
+        } catch (error) {
+            throw new Error(error)
+        }
+    }
+
+    public async refreshToken(
         userInputDTO: IUserInputDTO,
     ): Promise<{ user: IUser; token: string }> {
         try {
-
-            return 
-        } catch (e) {
-            console.error(e)
-            throw e
+            return
+        } catch (error) {
+            console.error(error)
+            throw error
         }
     }
 }
