54d612efbaf50add569db2deed6e87b839facd7b
Thevaleentiin <mikado93220@gmail.com>
2021-12-12 22:11:22 +0100

    update mealcard to mui

--- frontend/src/components/frontend/dashboard/listing/MealCard.js
+++ frontend/src/components/frontend/dashboard/listing/MealCard.js
@@ -1,15 +1,13 @@
-// import Grid from '@material-ui/core/Grid'
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-// import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import AccessTimeOutlinedIcon from '@material-ui/icons/AccessTimeOutlined';
-import EuroOutlinedIcon from '@material-ui/icons/EuroOutlined';
+import { makeStyles } from '@mui/styles';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined';
+import EuroOutlinedIcon from '@mui/icons-material/EuroOutlined';
 
 
 const useStyles = makeStyles({
