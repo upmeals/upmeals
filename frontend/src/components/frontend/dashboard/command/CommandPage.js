e9017c142d8bb999e884c913ed216be8647808c0
zkolait <chapenicolas@gmail.com>
2021-12-13 11:19:35 +0100

    link ap iback

--- frontend/src/components/frontend/dashboard/command/CommandPage.js
+++ frontend/src/components/frontend/dashboard/command/CommandPage.js
@@ -1,13 +1,42 @@
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import React from 'react';
-import Page from '../../../ui/Page'
+import Page from '../../../ui/Page';
+import useAllRecords from '../../../../hooks/useAllRecords';
 
 
 const CommandPage = () => {
+
+    const { loading: loadingRecipies, records: recipies } = useAllRecords('recipies');
+
     return (
         <Page>
             <Grid>
                 <p>Commander</p>
+
+                {
+                    loadingRecipies ? (
+                        <p>Loading...</p>
+                    ) : (
+                        <Grid
+                            container
+                        >
+                            {
+                                recipies.map((recipe, index) => {
+                                    return (
+                                        <Grid
+                                            container
+                                            key={index}
+                                        >
+                                            <Typography variant="body2">
+                                                {JSON.stringify(recipe)}
+                                            </Typography>
+                                        </Grid>
+                                    )
+                                })
+                            }
+                        </Grid>
+                    )
+                }
             </Grid>
         </Page>
     )
--- frontend/src/config/constants.js
+++ frontend/src/config/constants.js
@@ -1,7 +1,11 @@
 export const userApiHost = `https://api.${document.location.hostname}`;
+export const recipieApiHost = `https://api.${document.location.hostname}`;
+export const ingredientApiHost = `https://api.${document.location.hostname}`;
 
 export const servicesMap = {
     user: userApiHost,
+    recipie: recipieApiHost,
+    ingredient: ingredientApiHost,
 };
 
 export const languages = [['fr', 'language__french']];
