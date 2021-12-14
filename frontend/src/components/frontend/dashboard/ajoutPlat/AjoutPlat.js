02d8301b2be5be656828ec9f52c80e31b51f000c
jblahak <jblahak@easyvista.com>
2021-12-14 23:57:39 +0100

    add modifs review ajoutplat component

--- frontend/src/components/frontend/dashboard/ajoutPlat/AjoutPlat.js
+++ frontend/src/components/frontend/dashboard/ajoutPlat/AjoutPlat.js
@@ -4,74 +4,58 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import AddIcon from '@mui/icons-material/Add';
-import Modal from '@mui/material/Modal';
-import {Box} from "@mui/material";
+import {createStyles, makeStyles} from '@mui/styles';
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
+const useStyles = makeStyles(theme =>
+  createStyles({
+    card: {
+      width: '220px',
+      maxWidth: 220,
+      borderRadius: '20px',
+      cursor: 'pointer',
+    },
+    cardMedia: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '220px',
+      backgroundColor: '#F5F5F4',
+    },
+    addIcon: {
+      width: '1.5em',
+      height: '1.5em',
+      color: '#707070',
+    },
+    cardContent: {
+      paddingBottom: '12px !important',
+      paddingTop: '12px !important',
+    },
+    typographyCard: {
+      textAlign: 'center',
+      marginBottom: '0',
+    }
+  })
+)
 
 export default function AjoutPlat() {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
+  const classes = useStyles();
+
   return (
     <>
-      <Card onClick={handleOpen} sx={{
-        width: '220px',
-        maxWidth: 220,
-        borderRadius: '20px',
-        cursor: 'pointer',
-      }}>
-        <CardMedia sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '140px',
-          backgroundColor: '#F5F5F4',
-        }}>
-          <AddIcon sx={{
-            width: '2em',
-            height: '2em',
-            color: '#707070',
-          }}/>
+      <Card onClick={handleOpen} className={classes.card}>
+        <CardMedia className={classes.cardMedia}>
+          <AddIcon className={classes.addIcon}/>
         </CardMedia>
-        <CardContent sx={{
-          paddingBottom: '12px !important',
-          paddingTop: '12px !important',
-        }}>
-          <Typography gutterBottom variant="h5" component="div" sx={{
-            textAlign: 'center',
-            marginBottom: '0',
-          }}>
+        <CardContent className={classes.cardContent}>
+          <Typography gutterBottom variant="body1" component="div" className={classes.typographyCard}>
             Ajouter un plat
           </Typography>
         </CardContent>
       </Card>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
     </>
   );
 }
