772145d135fa8f8aec838df4e693029203a290f8
zkolait <chapenicolas@gmail.com>
2021-12-15 13:50:38 +0100

    merge w develop + quick fixes

--- frontend/src/components/frontend/dashboard/Dashboard.js
+++ frontend/src/components/frontend/dashboard/Dashboard.js
@@ -1,14 +1,13 @@
 import { Grid } from '@mui/material';
 import React from 'react';
 import Page from '../../ui/Page'
-import DetailPlat from "./detailPlat/DetailPlat";
 
 
 const Dashboard = () => {
     return (
         <Page>
             <Grid>
-              <DetailPlat />
+                Dashboard
             </Grid>
         </Page>
     )
