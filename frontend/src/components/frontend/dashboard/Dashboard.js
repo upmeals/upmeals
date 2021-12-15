dce0f28e2c3ec3b2298729f60c73e1c1108b30f9
zkolait <chapenicolas@gmail.com>
2021-12-15 09:28:03 +0100

    quick fixes

--- frontend/src/components/frontend/dashboard/Dashboard.js
+++ frontend/src/components/frontend/dashboard/Dashboard.js
@@ -2,13 +2,24 @@ import { Grid } from '@mui/material';
 import React from 'react';
 import Page from '../../ui/Page'
 import AjoutPlat from "./ajoutPlat/AjoutPlat";
+import MealCard from './command/MealCard';
 
 
 const Dashboard = () => {
     return (
         <Page>
             <Grid>
-              <AjoutPlat />
+                <AjoutPlat />
+                <MealCard
+                    recipe={
+                        {
+                            attributes: {
+                                title: 'titre'
+                            }
+                        }
+                    }
+                    index={1}
+                />
             </Grid>
         </Page>
     )
--- frontend/src/components/frontend/dashboard/ajoutPlat/AjoutPlat.js
+++ frontend/src/components/frontend/dashboard/ajoutPlat/AjoutPlat.js
@@ -1,61 +1,65 @@
-import React, {useState} from 'react'
+import React from 'react'
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import AddIcon from '@mui/icons-material/Add';
-import {createStyles, makeStyles} from '@mui/styles';
+import { createStyles, makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles(theme =>
-  createStyles({
-    card: {
-      width: '220px',
-      maxWidth: 220,
-      borderRadius: '20px',
-      cursor: 'pointer',
-    },
-    cardMedia: {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '220px',
-      backgroundColor: '#F5F5F4',
-    },
-    addIcon: {
-      width: '1.5em',
-      height: '1.5em',
-      color: '#707070',
-    },
-    cardContent: {
-      paddingBottom: '12px !important',
-      paddingTop: '12px !important',
-    },
-    typographyCard: {
-      textAlign: 'center',
-      marginBottom: '0',
-    }
-  })
+	createStyles({
+		root: {
+			width: '220px',
+			maxWidth: 220,
+			borderRadius: '20px',
+			cursor: 'pointer',
+			backgroundColor: "#FFF!important",
+            borderRadius: '20px !important',
+			'&:hover $cardMedia': {
+				background: '#F1F1F0',
+				transition: '0.25s !important',
+			},
+			'&:hover $cardContent': {
+				background: '#FBFBFA',
+				transition: '0.25s !important',
+			}
+		},
+		cardMedia: {
+			display: 'flex !important',
+			justifyContent: 'center',
+			alignItems: 'center',
+			height: '220px',
+			backgroundColor: '#F5F5F4',
+		},
+		addIcon: {
+			width: '1.5em !important',
+			height: '1.5em !important',
+			color: '#707070',
+		},
+		cardContent: {
+			paddingBottom: '12px !important',
+			paddingTop: '12px !important',
+		},
+		typographyCard: {
+			textAlign: 'center',
+			marginBottom: '0',
+		}
+	})
 )
 
 export default function AjoutPlat() {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+	const classes = useStyles();
 
-  const classes = useStyles();
-
-  return (
-    <>
-      <Card onClick={handleOpen} className={classes.card}>
-        <CardMedia className={classes.cardMedia}>
-          <AddIcon className={classes.addIcon}/>
-        </CardMedia>
-        <CardContent className={classes.cardContent}>
-          <Typography gutterBottom variant="body1" component="div" className={classes.typographyCard}>
-            Ajouter un plat
-          </Typography>
-        </CardContent>
-      </Card>
-    </>
-  );
+	return (
+		<Card className={classes.root}>
+			<CardMedia className={classes.cardMedia}>
+				<AddIcon className={classes.addIcon} />
+			</CardMedia>
+			<CardContent className={classes.cardContent}>
+				<Typography gutterBottom variant="body1" component="div" className={classes.typographyCard}>
+					Ajouter un plat
+				</Typography>
+			</CardContent>
+		</Card>
+	);
 }
